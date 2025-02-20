e8c6bf0e760d932d2c235f56fa00e9c487fe54b2
32Valik92 <strukalo.valentyn@gmail.com>
2025-02-20 02:29:21 +0200

    pagination

--- src/components/Products/ProductsComponent.tsx
+++ src/components/Products/ProductsComponent.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useEffect} from "react";
+import React, {FC, useEffect, useState} from "react";
 
 import {productAction, useAppDispatch, useAppSelector} from "../../redux";
 import {ProductComponent} from "../Product/ProductComponent";
@@ -8,27 +8,53 @@ const ProductsComponent: FC = () => {
    const {products, loading} = useAppSelector(state => state.productReducer);
    const dispatch = useAppDispatch();
 
+   const [currentPage, setCurrentPage] = useState<number>(1);
+   const productsPerPage = 4;
+
+   const indexLastProduct = currentPage * productsPerPage;
+   const indexFirstProduct = indexLastProduct - productsPerPage;
+   const currentProducts = products.slice(indexFirstProduct, indexLastProduct);
+
+   const prevPage = () => {
+      if (currentPage > 1) {
+         setCurrentPage(prev => prev - 1);
+      }
+   };
+
+   const nextPage = () => {
+      if (currentPage < Math.ceil(products.length / productsPerPage)) {
+         setCurrentPage(prev => prev + 1);
+      }
+   };
+
    useEffect(() => {
       dispatch(productAction.getProducts());
       dispatch(productAction.getCategories());
    }, [dispatch]);
 
    return (
-      <div>
+      loading ?
+         <div className="loader"></div>
+         :
+         <div className="productsAndPagination">
+            <div className="products">
+               {currentProducts.map((product) => (
+                  <ProductComponent key={product.id} product={product}/>
+               ))}
+            </div>
 
+            <div className="pagination">
+               <button onClick={prevPage} disabled={currentPage === 1}>
+                  {"<"} Prev
+               </button>
 
-         {
-            loading ?
-               <div className="loader"></div>
-               :
-               <div className="products">
-                  {products.map((product) => (
-                     <ProductComponent key={product.id} product={product}/>
-                  ))}
-               </div>
+               <p>Page {currentPage} of {Math.ceil(products.length / productsPerPage)}</p>
 
-         }
-      </div>
+               <button onClick={nextPage} disabled={currentPage === Math.ceil(products.length / productsPerPage)}>
+                  Next {">"}
+               </button>
+            </div>
+         </div>
    );
 
 };
